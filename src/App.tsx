c67ceafd39b2594d6ffcc0fa178aeae36d6261f7
apinkev <apinkevich@yahoo.com>
2020-07-04 21:09:33 +0300

    added random disabled inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 import PasswordInput from "./components/passwordInput";
+import "./App.css";
 
 const App = () => {
+    const [password, setPassword] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setEnteredPassword(true);
+    };
+
     return (
-        <div className="App">
-            <PasswordInput />
-        </div>
+        <>
+            {enteredPassword ? (
+                <PasswordInput password={password} />
+            ) : (
+                <div>
+                    <h3>Enter your password:</h3>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            maxLength={32}
+                            value={password}
+                            onChange={handleChange}
+                        />
+                        <input type="submit" />
+                    </form>
+                </div>
+            )}
+        </>
     );
 };
 
--- src/components/passwordInput.tsx
+++ src/components/passwordInput.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "../styles/passwordInput.css";
 
-const PasswordInput = () => {
-    return <div>Hello from password</div>;
+interface Props {
+    password: string;
+}
+
+const PasswordInput = ({ password }: Props) => {
+    const passwordArray = password.split("");
+    const inputArray: JSX.Element[] = [];
+
+    const getRandomDisabledInputs = () => {
+        const values = [];
+        for (let index = 0; index < 5; index++) {
+            const randomIndex = Math.floor(
+                Math.random() * Math.floor(passwordArray.length)
+            );
+            values.push(randomIndex);
+        }
+        return values;
+    };
+
+    const values = getRandomDisabledInputs();
+
+    for (let index = 0; index < 32; index++) {
+        const element = (
+            <input
+                key={index}
+                disabled={!values.includes(index)}
+                maxLength={1}
+                className={
+                    values.includes(index)
+                        ? "single-character"
+                        : "single-character disabled"
+                }
+            />
+        );
+        inputArray.push(element);
+    }
+
+    return <div className="password-container">{inputArray}</div>;
 };
 
 export default PasswordInput;
